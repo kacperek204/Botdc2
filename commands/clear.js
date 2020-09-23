b7ace5d2b6acbe68375694bbcbe5b6cd70b13141
kacperek204 <71221171+kacperek204@users.noreply.github.com>
2020-09-23 10:59:02 +0200

    Botdc2

--- commands/clear.js
+++ commands/clear.js
@@ -14,3 +14,15 @@ module.exports.run = async (bot, message, args) => {
 module.exports.help = {
   name: "clear",
 };
+
+if(command =="say"){
+  var embed = new Discord.MessageEmbed()
+  .setAuthor(message.member.user.username, message.member.user.avatarURL)
+  .setColor('#0099ff')
+  .setDescription('Bot')
+  message.delete()
+  if(message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(message.content.slice(prefix.length+3))
+  else 
+  return message.channel.send("Nie posiadasz permisji"), message.channel.send(embed)
+  
+};
