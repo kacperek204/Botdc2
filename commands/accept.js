77ab9c69eb4f9e467bf13b8dcb171ec936fb529c
kacperek204 <71221171+kacperek204@users.noreply.github.com>
2020-09-27 22:20:23 +0200

    a

---
+++ commands/accept.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js");
+const config = require("../config.json");
+
+module.exports.run = async(bot, message, args, msg) => {
+
+    let sicon = message.guild.name;
+let embeda = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setThumbnail(message.author.avatarURL)
+    .addField(`${message.member.displayName}`, `Odmowa dostępu`)
+    .setFooter(sicon)
+    .setTimestamp()
+    if (!message.member.roles.find(r => r.name === config.rolaaccept)) return message.channel.send(embeda);
+
+          const tekst = args.join(' ');
+          if (!tekst) return message.channel.send("Podaj tekst podania"); 
+          let reportEmbed = new Discord.RichEmbed() 
+            .setTitle("📝 Podanie rozpatrzone pozytywnie!") 
+            .setColor(`#4BFF1E`) 
+            .addField("Osoba sprawdzająca:", message.author)
+            .addField("Wynik", tekst)
+            .setFooter(sicon) 
+            .setThumbnail(message.author.avatarURL)
+            .setTimestamp() 
+             
+            message.delete().catch(O_o=>{});
+            message.guild.channels.get(config.idaccept).sendMessage(reportEmbed);
+        
+            return;     
+    }
+    
+    
+    module.exports.help = {
+    
+      name:`${config.komendaaccept}`
+    
+    }
