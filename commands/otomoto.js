5df5a25606177cf28d59742f4fc60104c0cd8016
kacperek204 <71221171+kacperek204@users.noreply.github.com>
2020-09-28 19:28:39 +0200

    Update otomoto.js

--- commands/otomoto.js
+++ commands/otomoto.js
@@ -5,28 +5,28 @@ module.exports.run = async(bot, message, args, msg) => {
         const filter = response => {
             return response.author.id === message.author.id
         };
-        message.reply(`Wpisz tytuł ogłoszenia`).then((w) => {
+        message.reply(`Wpisz tytuł ogłoszenia (Masz 3 minuty)`).then((w) => {
             message.channel.awaitMessages(filter, {
                 max: 1,
                 time: 300000,
                 errors: ['time']
             }).then(c => {
                 let title = c.first().content;
-                message.reply(`Wpisz treść ogłoszenia`).then((w) => {
+                message.reply(`Wpisz treść ogłoszenia (Masz 10 minut)`).then((w) => {
                     message.channel.awaitMessages(filter, {
                         max: 1,
                         time: 1000000,
                         errors: ['time']
                     }).then(de => {
                         let desc = de.first().content;
-                        message.reply(`Wpisz cene`).then((w) => {
+                        message.reply(`Wpisz cene (Masz minute)`).then((w) => {
                             message.channel.awaitMessages(filter, {
                                 max: 1,
                                 time: 1000000,
                                 errors: ['time']
                             }).then(a => {
                                 let price = a.first().content;    
-                                message.reply(`Wklej zdjęcia`).then((w) => {
+                                message.reply(`Wklej zdjęcia (Musisz wrzucić zdjęcie na imgura, a następnie wkleić link na kanał, masz 10 minut)`).then((w) => {
                                     message.channel.awaitMessages(filter, {
                                         max: 1,
                                         time: 1000000,
@@ -41,10 +41,10 @@ module.exports.run = async(bot, message, args, msg) => {
                                             .setColor("#D40F0A")
                                             .setThumbnail(message.author.avatarURL)
                                             .addField("Autor Ogłoszenia:", `${message.author}`)
-                                            .setImage(photo)
+                                            .addField("Zdjęcia pokazowe:", `${photo}`)
                                             .setTimestamp() 
                                             message.channel.send(embed)
-                                            message.channel.bulkDelete(7)
+                                            message.channel.bulkDelete(8)
                                             
                                     }).catch((e => {
                                         message.reply(`Czasu upłynął! Spróbuj jeszcze raz!`)
